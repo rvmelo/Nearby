cd93fb88407437a816fbbed6749f3caea6abb7df
Roberto Melo <rvmelo@inovahu.com>
2024-12-12 23:17:03 -0300

    first commit

---
+++ src/components/welcome/index.tsx
@@ -0,0 +1,14 @@
+import { Image, Text, View } from 'react-native'
+import { s } from './styles'
+
+export function Welcome() {
+  return (
+    <View>
+      <Image source={require('@/assets/logo.png')} style={s.logo} alt="" />
+      <Text style={s.title}>Boas vindas ao Nearby!</Text>
+      <Text style={s.subtitle}>
+        Tenha cupons de vantagem para usar em seus estabelecimentos favoritos.
+      </Text>
+    </View>
+  )
+}
